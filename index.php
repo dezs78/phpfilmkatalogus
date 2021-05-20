7335d4b5c93f8e1294130a810f870f3bc13ec311
Dezső Kupa <57774698+dezs78@users.noreply.github.com>
2021-05-20 15:50:05 +0200

    filmkatalogus

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE>
+<html lang=hu>
+<head>
+	<meta charset="utf8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<title>Filmkatalogus</title>
+</head>
+<body>
+	<div class="contanier">
+		<table class="table">
+			<thead>
+				<th>
+					Filmcím
+				</th>
+				<th>
+					Kategoria
+				</th>
+				<th>
+					Rendező
+				</th>
+				<th>
+					Főszereplő
+				</th>
+				<th>
+					Tartalom
+				</th>
+				<th>
+					Megjelenés éve
+				</th>
+				<th>
+					Film hossza
+				</th>
+			</thead>
+			<tbody>
+			
+			</tbody>
+		</table>
+	</div>
+
+</body>
+</html>
