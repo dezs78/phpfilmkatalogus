6339deacaa591b88157b2547bcd302bb32f1d995
Dezső Kupa <57774698+dezs78@users.noreply.github.com>
2021-05-21 17:43:00 +0200

    sql

--- connect.php
+++ connect.php
@@ -1,5 +1,20 @@
 <?php
 	
+	$cim = $_GET['cim'];
+	$kategoria = $_GET['kategoria'];
+	$rendezo = $_GET['rendezo'];
+	$foszereplo = $_GET['foszereplo'];
+	$tartalom = $_GET['tartalom'];
+	$ev = $_GET['ev'];
+	$hossz = $_GET['hossz'];
+	
 	$db = new mysqli('localhost', 'root', '', 'filmkatalog');
+	
+	$feltotles = "INSERT INTO film_katalog VALUES (NULL, '$cim', '$kategoria', '$rendezo', '$foszereplo', '$tartalom', '$ev', '$hossz')";
+	
+	$db->query($feltotles);
+	
+	$db->close();
+	
 
 ?>
